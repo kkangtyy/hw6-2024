16eec0737d35d3c5eed7f8925260b04216ab5940
Terri Kang <kangty@umich.edu>
2024-04-04 19:48:39 -0400

    Modified code from lecture

--- js/video.js
+++ js/video.js
@@ -1,7 +1,12 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
+	video = document.querySelector("#player1")
+	video.autoplay = false;
+	video.loop = false;
+	console.log("Auto play is set to " + video.autoplay)
+	console.log("Loop is set to " + video.loop)
 
 });
 
